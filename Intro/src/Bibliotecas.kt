0b2e892c89be110feb89e5e15f30b7840b857ca3
Tiago7mendes <tiagosettimendes@gmail.com>
2025-08-25 11:17:33 -0300

    exercicies

---
+++ Intro/src/Bibliotecas.kt
@@ -0,0 +1,29 @@
+import kotlin.random.Random
+import kotlin.math.*
+
+fun main() {
+    print("Digite sua idade: ")
+    val idade = readln().toIntOrNull() ?: 0
+    println("Você tem $idade anos")
+
+    val nome = " Kotlin "
+    println(nome.trim())
+    println(nome.uppercase())
+    println(nome.lowercase())
+    println(nome.length)
+    println("Olá, ${nome.trim()}!")
+
+    println(abs(-10)) // math
+    println(sqrt(16.0))
+    println(pow(2.0, 3.0))
+    println(round(3.7))
+    println(hypot(3.0, 4.0))
+
+    println(Random.nextInt(10)) // random
+    println((1..6).random())
+
+    //● Random.nextInt(n) → número entre 0 e n-1
+    //● Random.nextInt(a, b) → número entre a e b-1
+    //● Random.nextDouble() → número decimal entre 0.0 e 1.0
+    //● (range).random() → valor aleatório dentro de um intervalo
+}
---
+++ Intro/src/Ex4.kt
@@ -0,0 +1,44 @@
+import random.Random
+import kotlin.math.*
+fun main() {
+    // Você está programando um jogo e precisa, em todo frame, calcular a distância entre o
+    //personagem do(a) jogador(a) com um inimigo. Dados os pontos cartesianos P1 = (x1, y1)
+    //representando a localização do(a) jogador(a) e P2 = (x2, y2) representando a posição do
+    //inimigo, crie um programa que calcule a distância entre estes dois pontos. Caso o
+    //inimigo esteja há uma distância menor ou igual a 5, você deverá mostrar “[ENTRAR NO
+    //MODO ATAQUE]”. O cálculo da distância é feito pela fórmula ao lado, em que d representa
+    //a distância. Gere números aleatórios entre 0 e 10 para x1, x2, y1 e y2.
+
+    val frames = 5
+
+    for (i in 0..frames) {
+        val dist = jogo()
+        println("A distancia do inimigo é ${dist}")
+        if (dist <= 5){
+            println("ENTRAR NO MODO ATAQUE")
+        }
+    }
+}
+
+fun jogo(): Double {
+    val x1 = (0..9).random()
+    val y1 = (0..9).random()
+    val x2 = (0..9).random()
+    val y2 = (0..9).random()
+
+    val matriz = 10
+
+    for (i in 1..matriz){
+        for (j in 1..matriz){
+            if (x1 == i && y1 == j){
+                print("@ ") // heroi
+            } else if (x2 == i && y2 == j){
+                print("# ") // inimigo
+            } else print(". ")
+        }
+        println()
+    }
+
+    val d = sqrt((x2 - x1).toDouble().pow(2) + (y2 - y1).toDouble().pow(2))
+    return d
+}
